9fc72eb5753dd7c6e402cf4cab99d23a0ed0c082
ichimaru shoma <openthe469@gmail.com>
2020-08-25 18:07:09 +0900

    20200825

---
+++ server/todo_app/resources/views/layouts/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <h1>TODO_app</h1>
+        <p> 編集画面</p>
+    </body>
+</html>
--- server/todo_app/routes/web.php
+++ server/todo_app/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todos', 'TodosController@index')->name('todos.index');
+Route::post('todos', 'TodosController@store')->name('todos.store');
+Route::put('todos/{id}', 'TodosController@update')->name('todos.update');
+Route::get('todos/{id}', 'TodosController@show')->name('todos.show');
+Route::delete('todos/{id}', 'TodosController@destroy')->name('todos.destroy');
+Route::get('todos/create', 'TodosController@create')->name('todos.create');
+Route::get('todos/{id}/edit', 'TodosController@edit')->name('todos.edit');
