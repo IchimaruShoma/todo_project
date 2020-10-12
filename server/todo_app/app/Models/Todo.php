e69c9e2752a1f2bfc241d68275c7541416138f6f
ichimaru shoma <openthe469@gmail.com>
2020-10-13 01:01:57 +0900

    Add some Model file

--- server/todo_app/app/Models/Todo.php
+++ server/todo_app/app/Models/Todo.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Todo extends Model
-{
-    protected $table = 'todo';
+class Todo extends Model{
+    protected $table = 'todos';
+
+    protected $fillable = [
+        'name',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'complete' => 'boolean',
+    ];
 }
