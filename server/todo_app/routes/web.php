8c296b51a7f007e4417981d51ee34210c4cd5956
ichimaru shoma <openthe469@gmail.com>
2020-10-13 01:01:30 +0900

    Fix routing file

--- server/todo_app/routes/web.php
+++ server/todo_app/routes/web.php
@@ -2,25 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Home Page
+Route::get('/', 'AuthController@home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Login and Logout
+Route::get('/login', ['middleware' => 'guest', 'uses' => 'AuthController@getLogin']);
+Route::post('/login', ['middleware' => 'guest', 'uses' => 'AuthController@postLogin']);
+Route::get('/logout', ['middleware' => 'auth', 'uses' => 'AuthController@logout']);
 
-Route::get('todos', 'TodosController@index')->name('todos.index');
-Route::post('todos', 'TodosController@store')->name('todos.store');
-Route::put('todos/{id}', 'TodosController@update')->name('todos.update');
-Route::get('todos/{id}', 'TodosController@show')->name('todos.show');
-Route::delete('todos/{id}', 'TodosController@destroy')->name('todos.destroy');
-Route::get('todos/create', 'TodosController@create')->name('todos.create');
-Route::get('todos/{id}/edit', 'TodosController@edit')->name('todos.edit');
+// Registration and User Profile
+Route::resource('users', 'UserController', ['except' => ['index', 'show', 'destroy']]);
+
+// Todo Resources
+Route::resource('todo', 'TodoController', ['middleware' => 'auth']);
