f2e302a8d043d34663800276eec3273893a4666e
ichimaru shoma <openthe469@gmail.com>
2020-10-13 01:02:48 +0900

    Add some Request files

---
+++ server/todo_app/app/Http/Requests/UserRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UserRequest extends Request
+{
+    public function authorize(){
+        return true;
+    }
+
+    public function rules(){
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ];
+    }
+}
